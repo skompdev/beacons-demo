4f3d82f0a955c177e0f17e6c12eea9fecc6d1950
skompdev <skompdev@skomp.net>
2014-03-02 14:10:18 -0600

    master

---
+++ AT_BlankScreenB.m
@@ -0,0 +1,146 @@
+/*
+ *	Copyright 2014, 50 APP fuse
+ *
+ *	All rights reserved.
+ *
+ *	Redistribution and use in source and binary forms, with or without modification, are 
+ *	permitted provided that the following conditions are met:
+ *
+ *	Redistributions of source code must retain the above copyright notice which includes the
+ *	name(s) of the copyright holders. It must also retain this list of conditions and the 
+ *	following disclaimer. 
+ *
+ *	Redistributions in binary form must reproduce the above copyright notice, this list 
+ *	of conditions and the following disclaimer in the documentation and/or other materials 
+ *	provided with the distribution. 
+ *
+ *	Neither the name of David Book, or buzztouch.com nor the names of its contributors 
+ *	may be used to endorse or promote products derived from this software without specific 
+ *	prior written permission.
+ *
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ *	IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ *	INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ *	NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ *	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ *	WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ *	OF SUCH DAMAGE. 
+ */
+
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "JSON.h"
+#import "BT_application.h"
+#import "BT_strings.h"
+#import "BT_viewUtilities.h"
+#import "BT_appDelegate.h"
+#import "BT_item.h"
+#import "BT_debugger.h"
+#import "BT_viewControllerManager.h"
+#import "AT_BlankScreenB.h"
+
+@interface AT_BlankScreenB ()
+
+@end
+
+
+@implementation AT_BlankScreenB
+
+//viewDidLoad
+-(void)viewDidLoad{
+	[BT_debugger showIt:self theMessage:@"viewDidLoad"];
+	[super viewDidLoad];
+
+
+	//Ibeacon UUID
+    // Create a NSUUID object
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"174BC4DE-8970-486C-A773-D99F563CD06C"];
+
+    
+    // Initialize the Beacon Region
+    self.myBeaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                                  major:1
+                                                                  minor:2
+                                                             identifier:@"com.BT.com"];
+    
+    
+    
+}
+
+//view will appear
+-(void)viewWillAppear:(BOOL)animated{
+	[super viewWillAppear:animated];
+	[BT_debugger showIt:self theMessage:@"viewWillAppear"];
+	
+	//flag this as the current screen
+	BT_appDelegate *appDelegate = (BT_appDelegate *)[[UIApplication sharedApplication] delegate];	
+	appDelegate.rootApp.currentScreenData = self.screenData;
+	
+	//setup navigation bar and background
+	[BT_viewUtilities configureBackgroundAndNavBar:self theScreenData:[self screenData]];
+
+	
+	
+}
+
+
+- (IBAction)buttonClicked:(id)sender {
+    
+    // Get the beacon data to advertise
+    self.myBeaconData = [self.myBeaconRegion peripheralDataWithMeasuredPower:nil];
+    
+    // Start the peripheral manager
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                     queue:nil
+                                                                   options:nil];
+}
+
+-(void)peripheralManagerDidUpdateState:(CBPeripheralManager*)peripheral
+{
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn)
+    {
+        // Bluetooth is on
+        
+        // Update our status label
+        self.statusLabel.text = @"Broadcasting your iBeacon....";
+        
+        // Start broadcasting
+        [self.peripheralManager startAdvertising:self.myBeaconData];
+    }
+    else if (peripheral.state == CBPeripheralManagerStatePoweredOff)
+    {
+        // Update our status label
+        self.statusLabel.text = @"Stopped";
+        
+        // Bluetooth isn't on. Stop broadcasting
+        [self.peripheralManager stopAdvertising];
+    }
+    else if (peripheral.state == CBPeripheralManagerStateUnsupported)
+    {
+        self.statusLabel.text = @"Unsupported - either using a simulator or Bluetooth off on your device!";
+    }
+}
+
+
+
+
+
+//dealloc
+-(void)dealloc {
+    [super dealloc];
+
+}
+
+
+@end
+
+
+
+
+
+
+
