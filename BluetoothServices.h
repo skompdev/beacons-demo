1d7b7fc1f0d8485c261a23d98dc3a31e25a7d632
skompdev <skompdev@skomp.net>
2014-03-02 11:49:16 -0600

    master

---
+++ BluetoothServices.h
@@ -0,0 +1,12 @@
+//
+//  BluetoothServices.h
+//  Broadcaster
+//
+//  Created by Lukasz Kostka on 8/1/13.
+//  Copyright (c) 2013 Estimote, Inc. All rights reserved.
+//
+
+// Run Terminal app, run uuidgen (twice) to get your very own UUIDs
+
+#define SERVICE_UUID @"174BC4DE-8970-486C-A773-D99F563CD06C"
+#define CHARACTERISTIC_UUID @"C2C65540-FD11-4E09-9689-FF3F2E57806F"
